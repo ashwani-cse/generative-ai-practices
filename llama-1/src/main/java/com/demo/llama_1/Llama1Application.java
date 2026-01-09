eef1bd459388a37377a81be7312c93b5ffe52582
Ashwani Kumar <ashwanikumar@Ashwanis-Laptop.local>
2026-01-09 15:51:49 +0530

    ollama model example

---
+++ llama-1/src/main/java/com/demo/llama_1/Llama1Application.java
@@ -0,0 +1,13 @@
+package com.demo.llama_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Llama1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Llama1Application.class, args);
+	}
+
+}
---
+++ llama-1/src/test/java/com/demo/llama_1/Llama1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.llama_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Llama1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
