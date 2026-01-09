9875ca39fd5d185620d847af0776ca1932b073aa
Ashwani Kumar <ashwanikumar@Ashwanis-Laptop.local>
2026-01-09 16:04:04 +0530

    multi model example

---
+++ multi-model-1/src/main/java/com/demo/multi_model_1/MultiModel1Application.java
@@ -0,0 +1,13 @@
+package com.demo.multi_model_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModel1Application.class, args);
+	}
+
+}
---
+++ multi-model-1/src/test/java/com/demo/multi_model_1/MultiModel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.multi_model_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
