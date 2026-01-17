a203909e5089e6cb52892170f6ee9e11741440ca
Ashwani Kumar <ashwanikumar@Ashwanis-Laptop.local>
2026-01-17 22:50:30 +0530

    control the max messages in chat memory

--- openai-prompt-template/src/main/java/com/demo/openai/config/ChatMemoryChatClientConfig.java
+++ openai-prompt-template/src/main/java/com/demo/openai/config/ChatMemoryChatClientConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,16 @@ import java.util.List;
  */
 @Configuration
 public class ChatMemoryChatClientConfig {
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory
+                .builder()
+                .maxMessages(2) // default is 20 in MessageWindowChatMemory
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+        return chatMemory;
+    }
 
     @Bean("chatMemoryChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
